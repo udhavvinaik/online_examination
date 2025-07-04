605fedea6c9281eba34023522d7d1c8236eb8ac5
udhavvinaik <udhavvinaik@gmail.com>
2025-07-04 11:26:24 +0530

    api

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 const apiBase = process.env.REACT_APP_API_BASE_URL;
-const BASE_URL = `${apiBase}/api/auth`;
 
-export const login = (data) => axios.post(`${BASE_URL}/login`, data);
+export const login = (data) => axios.post(`${apiBase}/api/auth/login`, data);
 
-export const studentSignup = (data) => axios.post(`${BASE_URL}/student/signup`, data);
+export const studentSignup = (data) => axios.post(`${apiBase}/api/auth/student/signup`, data);
 
-export const teacherSignup = (data) => axios.post(`${BASE_URL}/teacher/signup`, data);
+export const teacherSignup = (data) => axios.post(`${apiBase}/api/auth/teacher/signup`, data);
